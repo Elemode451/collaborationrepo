9b75b32f1068c0e43c51b41cef2d6d5534a166c6
egallop <77749795+egallop@users.noreply.github.com>
2021-03-25 13:06:18 -0700

    Update Collaborative Code.py

--- Main Code/Collaborative Code.py
+++ Main Code/Collaborative Code.py
@@ -4,7 +4,7 @@ arcade.open_window(800,800, "Cool Window that Does Stuff")
 arcade.start_render()
 arcade.set_background_color(arcade.csscolor.ORCHID)
 # drawing ground
-arcade.draw_lrtb_rectangle_filled(0, 800, 250, 0, arcade.csscolor.PURPLE)
+arcade.draw_lrtb_rectangle_filled(0, 800, 225, 0, arcade.csscolor.PURPLE)
 
 arcade.finish_render()
 arcade.run()
